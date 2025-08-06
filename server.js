943ba3899c0d7aa8759a61cd8dffc261e375d794
AndyyVasquez <andyy_vasquez_2102@hotmail.com>
2025-08-06 14:16:55 -0600

    update

--- server.js
+++ server.js
@@ -3069,7 +3069,7 @@ app.post('/api/login', async (req, res) => {
     try {
       // Buscar en administradores
       const [adminResults] = await connection.execute(
-        'SELECT id_admin as id, tipo_usu, nombre_admin as nombre, correo_admin as correo, password_admin as password FROM administradores WHERE correo_admin = ?',
+        'SELECT id_admin as id, tipo_usu, nombre_admin as nombre, correo as correo, password_admin as password FROM administradores WHERE correo = ?',
         [correo]
       );
 
